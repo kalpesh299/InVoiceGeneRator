559f94363c1b3d7bb535eb31f77c0dc22ca3f79e
Kalpesh Patil <72344646+kalpesh299@users.noreply.github.com>
2023-11-24 21:57:05 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-screen flex items-center justify-center ">
+     
+         <div className=' border-2 shadow-lg w-full p-1 md:w-2/4'>
+          {/* Header */}
+          <div className="flex justify-between border-2 border-red-200 ">
+           <h1 className='border-2 '>INVOICER</h1>
+           
+            <div className='border-black border-2 flex flex-col'>
+              <div className=" flex ">
+              <button className='mx-2 px-2 p-1 bg-blue-500 rounded-lg'>Print</button>
+             <button className='mx-2 px-2 p-1 bg-red-500 rounded-lg'>Download</button>
+             <button className='mx-2 px-2 p-1 bg-green-500 rounded-lg'>Send</button>
+              </div>
+             <h2 className="text-end mr-2 text-3xl font-bold pt-2">Kalpesh Patil</h2>
+             <p className="text-end mr-2 text-l ">Kalyan,Mumbai</p>
+           
+            </div>
+          </div>
+
+          <div>
+            client info
+          </div>
+
+         </div>
+        
+
+
     </div>
   );
 }
