abfbbe49def1f07d3729a8a59918c5cdbb245c7a
Kalpesh Patil <72344646+kalpesh299@users.noreply.github.com>
2023-11-25 12:38:43 +0530

    form stayle changed

--- src/App.js
+++ src/App.js
@@ -38,58 +38,71 @@ const print=()=>{
         <Footer mail={mail}  phno={phno} bankname={bankname} acch={acch} accno={accno} website={website} setpreview={setpreview} />
           </div>:
           <div className='md:mt-4 border-2 shadow-lg w-full p-2 flex flex-col  md:w-2/4 '>
-            <h1 className='text-center'>Form</h1>
-       <div className='flex md:justify-between md:flex-row flex-col'>
-       <label for='name' className='m-2'>Name :</label>
-            <input autoComplete='off' value={name} type='text' className='border-2 border-gray-500 mb-1 w-4/5 m-2' onChange={(e)=>{setname(e.target.value)}}/>
+            <h1 className='text-center font-bold text-xl'>Form</h1>
+            <div className='md:grid grid-cols-3 gap-5 md:mt-5'>
+
+            <div className='flex md:justify-between  flex-col'>
+       <label for='name' className=''>Name :</label>
+            <input autoComplete='off' value={name} type='text' className='border-2 border-gray-500 mb-1 w-4/5 ' onChange={(e)=>{setname(e.target.value)}}/>
        </div>
-       <div className='flex justify-between md:flex-row flex-col '>
-            <label for='email' className='m-2'>Email :</label>
-            <input type='email' value={mail}  className='border-2 border-gray-500 mb-1 w-4/5 md:m-2 ' onChange={(e)=>{setmail(e.target.value)}}/>
+       <div className='flex justify-between  flex-col '>
+            <label for='email' className=''>Email :</label>
+            <input type='email' value={mail}  className='border-2 border-gray-500 mb-1 w-4/5  ' onChange={(e)=>{setmail(e.target.value)}}/>
             </div>
-            <div className='flex justify-between md:flex-row flex-col'>
-            <label for='phone no' className='m-2'>Phone Number :</label>
-            <input type='number' value={phno}  className='border-2 border-gray-500 mb-1 w-4/5 m-2' onChange={(e)=>{setphno(e.target.value)}}/>
+            <div className='flex justify-between  flex-col'>
+            <label for='phone no' className=''>Phone Number :</label>
+            <input type='number' value={phno}  className='border-2 border-gray-500 mb-1 w-4/5 ' onChange={(e)=>{setphno(e.target.value)}}/>
             </div>
-            <div className='flex justify-between md:flex-row flex-col'>
-            <label for='bank' className='m-2'>Banck Name :</label> 
-            <input type='text' value={bankname}  className='border-2 border-gray-500 mb-1 w-4/5 m-2' onChange={(e)=>{setbankname(e.target.value)}}/>
             </div>
-            <div className='flex justify-between md:flex-row flex-col'>
-            <label for='acc holder' className='m-2'>Acc Holder :</label>
-            <input type='text' value={acch}  className='border-2 border-gray-500 mb-1 w-4/5 m-2' onChange={(e)=>{setacch(e.target.value)}}/>
+      
+            <div className='md:grid grid-cols-3 gap-10 md:mt-2'>
+            <div className='flex   flex-col'>
+            <label for='bank' className=''>Banck Name :</label> 
+            <input type='text' value={bankname}  className='border-2 border-gray-500 mb-1 w-4/5 ' onChange={(e)=>{setbankname(e.target.value)}}/>
             </div>
-            <div className='flex justify-between md:flex-row flex-col'>
+            <div className='flex   flex-col'>
+            <label for='acc holder' className=''>Acc Holder :</label>
+            <input type='text' value={acch}  className='border-2 border-gray-500 mb-1 w-4/5 ' onChange={(e)=>{setacch(e.target.value)}}/>
+            </div>
+            <div className='flex justify-between flex-col'>
             
-            <label for='acc no' className='m-2'>Acc No :</label>
-            <input type='number' value={accno}  className='border-2 border-gray-500 mb-1 w-4/5 m-2' onChange={(e)=>{setaccno(e.target.value)}}/>
+            <label for='acc no' className=''>Acc No :</label>
+            <input type='number' value={accno}  className='border-2 border-gray-500 mb-1 w-4/5 ' onChange={(e)=>{setaccno(e.target.value)}}/>
             </div>
-            <div className='flex justify-between md:flex-row flex-col'>
-            <label for='website' className='m-2'>Your Website :</label>
-            <input type='text' value={website}  className='border-2 border-gray-500 mb-1 w-4/5 m-2 ' onChange={(e)=>{setwebsite(e.target.value)}}/>
+            </div>
+           
+          
+            <div className='flex justify-between flex-col'>
+            <label for='website' className=''>Your Website :</label>
+            <input type='text' value={website}  className='border-2 border-gray-500 mb-1 w-11/12 ' onChange={(e)=>{setwebsite(e.target.value)}}/>
             </div>
           
-                <h1 className='text-center'>Client Details :</h1>
-                <div className='flex justify-between md:flex-row flex-col'>
-            <label for="" className='m-2'>Client Name :</label>
-            <input type='text' value={cliname}  className='border-2 border-gray-500 mb-1 w-4/5 m-2 ' onChange={(e)=>{setcliname(e.target.value)}}/>
+                <h1 className='text-center font-bold text-xl'>Client Details</h1>
+                <div className='flex justify-between  flex-col'>
+            <label for="" className='mt-2'>Client Name :</label>
+            <input type='text' value={cliname}  className='border-2 border-gray-500 mb-1 w-11/12  ' onChange={(e)=>{setcliname(e.target.value)}}/>
             </div>
-            <div className='flex justify-between md:flex-row flex-col'>
-            <label for="" className='m-2'>Client Mail :</label>
-            <input type='text' value={climail}  className='border-2 border-gray-500 mb-1 w-4/5 m-2 ' onChange={(e)=>{setclimail(e.target.value)}}/>
+            <div className='flex justify-between flex-col'>
+            <label for="" className=''>Client Mail :</label>
+            <input type='text' value={climail}  className='border-2 border-gray-500 mb-1 w-11/12   ' onChange={(e)=>{setclimail(e.target.value)}}/>
             </div>
-            <div className='flex justify-between md:flex-row flex-col'>
-            <label for="" className='m-2'>Invoice No :</label>
-            <input type='text' value={invno}  className='border-2 border-gray-500 mb-1 w-4/5 m-2 ' onChange={(e)=>{setinvno(e.target.value)}}/>
+            <div className='md:grid  grid-cols-3 gap-5 md:mt-20'>
+ 
+            <div className='flex   flex-col'>
+            <label for="" className='font-bold'>Invoice No :</label>
+            <input type='text' value={invno}  className='border-2 border-gray-500 px-2  w-4/5  ' onChange={(e)=>{setinvno(e.target.value)}}/>
             </div>
-            <div className='flex justify-between md:flex-row flex-col'>
-            <label for="" className='m-2'>Invoice Date :</label>
-            <input type='date' value={invd}  className='border-2 border-gray-500 mb-1 w-4/5 m-2 ' onChange={(e)=>{setinvd(e.target.value)}}/>
+            <div className='flex   flex-col'>
+            <label for="" className='font-bold'>Invoice Date :</label>
+            <input type='date' value={invd}  className='border-2 border-gray-500 px-2  w-4/5  ' onChange={(e)=>{setinvd(e.target.value)}}/>
             </div>
-            <div className='flex justify-between md:flex-row flex-col'>
-            <label for="" className='m-2'>Due Date :</label>
-            <input type='date' value={duedate}  className='border-2 border-gray-500 mb-1 w-4/5 m-2 ' onChange={(e)=>{setduedate(e.target.value)}}/>
+            <div className='flex   flex-col'>
+            <label for="" className='font-bold'>Due Date :</label>
+            <input type='date' value={duedate}  className='border-2 border-gray-500 px-2  w-4/5  ' onChange={(e)=>{setduedate(e.target.value)}}/>
             </div>
+
+            </div>
+            
             <label className='m-2'>Notes :</label>
             <textarea cols='50' rows='10' value={note} className='border-2 border-gray-500 mb-1 w-4/5 m-2 block m-auto' onChange={(e)=>{setnote(e.target.value)}}>
 
